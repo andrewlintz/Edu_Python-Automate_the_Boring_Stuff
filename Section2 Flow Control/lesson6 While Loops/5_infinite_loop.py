28bb60725b2fbff5b346bb0dc2e8e091e44ff8a3
ATXcloud <Andrew.j.lintz@gmail.com>
2017-12-03 15:01:01 -0600

    First commit

---
+++ Section2 Flow Control/lesson6 While Loops/5_infinite_loop.py
@@ -0,0 +1,4 @@
+#infinite Loop can be broke by keying in ctlr+c
+
+while True:
+    print('This is an infinite loop. Type "ctrl+c" to break the loop')
---
+++ Section2 Flow Control/lesson7 For Loops/4_While_Loop_Example_fivetimes.py
@@ -0,0 +1,7 @@
+print('My name is')
+i = 0
+while i < 5:
+#below was the For Loop Example
+#for i in range(5):
+    print('Jimmy Five Times ' + str(i))
+    i = i + 1
